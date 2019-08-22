befe6c404af24d5f82d0b35148bbeace1d91695e
Tom Rawlings <TomRawlings@vttech.local>
2019-08-22 09:09:18 +0100

    Scrolling & Animations

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // Sticky Navigation
     $('.js-section-features').waypoint(function(direction){
         if (direction == 'down') {
             $('nav').addClass('sticky');
@@ -10,12 +11,31 @@ $(document).ready(function(){
         offset: '60px'
     });
 
+    $('.js-scroll-to-plans').on('click', function(){
+        $('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 1000)
+    });
 
+    $('.js-scroll-to-start').on('click', function(){
+        $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000)
+    });
 
-    // var waypoints = $('#handler-first').waypoint(function(direction) {
-    //     notify(this.element.id + ' hit 25% from top of window') 
-    //   }, {
-    //     offset: '25%'
-    //   })
+    // Animations on Scroll
+
+    $('.js-wp-1').waypoint(function(direction){
+        $('.js-wp-1').addClass('animated fadeIn');
+    }, {offset: '50%'})
+
+    $('.js-wp-2').waypoint(function(direction){
+        $('.js-wp-2').addClass('animated fadeInUp');
+    }, {offset: '50%'})
+
+    $('.js-wp-3').waypoint(function(direction){
+        $('.js-wp-3').addClass('animated fadeIn');
+    }, {offset: '50%'})
+
+    $('.js-wp-4').waypoint(function(direction){
+        $('.js-wp-4').addClass('animated rubberBand');
+    }, {offset: '50%'})
+
 
 });
